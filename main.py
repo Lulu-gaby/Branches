3c221ae0beb510d3b3f02b53517a79d0a8075c1b
Lulu-gaby <lulugabyxxx@gmail.com>
2025-02-20 21:11:06 +0300

    add new code for entry

--- main.py
+++ main.py
@@ -11,4 +11,11 @@ root.title("Бла бла")
 label = tk.Label(root,text = "Введите имя:")
 label.pack()
 
+entry = tk.Entry(root)
+entry.pack()
+
+button = tk.Button(root, text = "Давай знакомиться!", command=user_name)
+button.pack()
+
+
 root.mainloop()
